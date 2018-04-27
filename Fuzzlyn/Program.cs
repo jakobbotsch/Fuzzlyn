8224da0db8a019878eeec42409393aee544f1aec
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-04-28 00:22:24 +0200

    Initial commit of .NET Core project

---
+++ Fuzzlyn/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+
+namespace Fuzzlyn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = SyntaxFactory.ParseCompilationUnit("namespace A { class B { void C() { } } }");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
