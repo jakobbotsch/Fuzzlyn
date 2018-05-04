ce8064635e05f64c8a8873dc9313b2e88ceff8f5
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-05-04 23:28:42 +0200

    Tiny bit of work on expressions...

--- Fuzzlyn/RandomExpressionGenerator.cs
+++ Fuzzlyn/RandomExpressionGenerator.cs
@@ -1,6 +1,27 @@
-﻿namespace Fuzzlyn
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Fuzzlyn
 {
     partial class RandomSyntaxGenerator
     {
+        [Recursive]
+        public AnonymousMethodExpressionSyntax GenAnonymousMethodExpression(ParameterListSyntax parameters, CSharpSyntaxNode body)
+            => AnonymousMethodExpression(parameters, body);
+
+        /*public AnonymousMethodExpressionSyntax GenAnonymousAsyncMethodExpression(ParameterListSyntax parameters, BlockSyntax body)
+            => AnonymousMethodExpression(parameters, body).WithAsyncKeyword(Token(SyntaxKind.AsyncKeyword));*/
+
+        [Recursive]
+        public SimpleLambdaExpressionSyntax GenSimpleLambdaExpressionWithBlock(ParameterSyntax parameter, CSharpSyntaxNode body)
+            => SimpleLambdaExpression(parameter, body);
+
+        [Recursive]
+        public SimpleLambdaExpressionSyntax GenSimpleAsyncLambdaExpressionWithBlock(ParameterSyntax parameter, CSharpSyntaxNode body)
+            => SimpleLambdaExpression(parameter, body).WithAsyncKeyword(Token(SyntaxKind.AsyncKeyword));
+
+        /*public SimpleLambdaExpressionSyntax GenSimpleLambdaExpressionWithArrow(ParameterSyntax parameter, ArrowExpressionClauseSyntax body)
+            => SimpleLambdaExpression(parameter, body)*/
     }
 }
