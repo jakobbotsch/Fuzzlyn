f47fdfac240dbea1db41a4b309d37e85d6a6d57d
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:39:59 +0200

    Simplify with SimplifyConstant

--- example.cs
+++ example.cs
@@ -116,7 +116,7 @@ public class Program
     static short s_113 = 1;
     static byte s_114 = 236;
     static short[] s_115 = new short[]{-32768};
-    static long s_116 = 5852970427657993206L;
+    static long s_116 = 0;
     static sbyte[][] s_117 = new sbyte[][]{new sbyte[]{-127, -128, 0, -10, 126, -1, -23}, new sbyte[]{1, -2, -128, -93}, new sbyte[]{98, -7, 0, 1, 127, -41, 51}, new sbyte[]{103, 127, -123, 126, 110, 126}, new sbyte[]{0}, new sbyte[]{0}};
     static bool s_118 = true;
     static short s_119 = 0;
