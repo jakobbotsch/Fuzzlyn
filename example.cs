9606a83b44006d0ae794434a2f3778be064c8d08
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:37:43 +0200

    Simplify with SimplifyStatementRemove

--- example.cs
+++ example.cs
@@ -198,7 +198,6 @@ public class Program
     {
         sbyte var0 = default(sbyte);
         ushort var6 = default(ushort);
-        int var10 = default(int);
         uint var11 = default(uint);
         byte var12 = default(byte);
         bool var13 = default(bool);
