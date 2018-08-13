ad019021c3c8d44894821505414db7361fed1027
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:39:53 +0200

    Simplify with SimplifyInitializer

--- example.cs
+++ example.cs
@@ -85,7 +85,7 @@ public class Program
     static ushort s_82 = 0;
     static int[, ] s_83 = new int[, ]{{0}};
     static long[] s_84 = new long[]{0};
-    static ushort[] s_85 = new ushort[]{0, 33956, 5112};
+    static ushort[] s_85 = new ushort[]{0};
     static ulong[][][][] s_86 = new ulong[][][][]{new ulong[][][]{new ulong[][]{new ulong[]{9473705828934257457UL, 11681380003416049046UL, 2UL}}}};
     static bool s_87 = true;
     static ushort[] s_88 = new ushort[]{63268, 11840, 58711, 18436, 1, 45518, 1, 16752, 0};
