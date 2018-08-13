ba044c0dc36015455b945731ef2cafd0340d2d28
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:37:06 +0200

    Simplify with SimplifyStatementRemove

--- example.cs
+++ example.cs
@@ -577,7 +577,6 @@ public class Program
 
     static ref ulong M56()
     {
-        long[, ] var3 = default(long[, ]);
         uint var4 = default(uint);
         ushort var5 = default(ushort);
         bool var6 = default(bool);
