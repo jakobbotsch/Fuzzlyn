2240c9e229c806de14fff905a98574c001cf9b85
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:40:16 +0200

    Simplify with RemoveFieldDeclaration

--- example.cs
+++ example.cs
@@ -104,7 +104,6 @@ public class Program
     static bool s_118 = true;
     static byte[][] s_120 = new byte[][]{new byte[]{0}};
     static short[][][] s_121 = new short[][][]{new short[][]{new short[]{0}}};
-    static bool s_122 = false;
     static byte s_123 = 0;
     static uint s_124 = 0;
     public static void Main(Fuzzlyn.Execution.IRuntime rt)
