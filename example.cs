a9cc39d65f3ba88362bd5f0b08eded3dab681f52
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:40:27 +0200

    Simplify with RemoveFieldDeclaration

--- example.cs
+++ example.cs
@@ -63,7 +63,6 @@ public class Program
     static short s_113 = 0;
     static short[] s_115 = new short[]{0};
     static sbyte[][] s_117 = new sbyte[][]{new sbyte[]{0}};
-    static bool s_118 = true;
     static short[][][] s_121 = new short[][][]{new short[][]{new short[]{0}}};
     static byte s_123 = 0;
     static uint s_124 = 0;
