2e237e1e1716a2941e2aac834df453cdff9c4dc5
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:39:13 +0200

    Simplify with SimplifyBinaryExpressionToLeft

--- example.cs
+++ example.cs
@@ -191,7 +191,7 @@ public class Program
     static long M11(uint[] arg0)
     {
         M12(s_16, s_25, s_19);
-        return M47((long)(arg0[0] / (ulong)(0UL | 1)));
+        return M47((long)(arg0[0]));
     }
 
     static byte M12(sbyte[, ] arg0, byte arg1, short arg2)
