230772cda469915ba517b3120088bd1cfb9b8719
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:39:45 +0200

    Simplify with SimplifyConstant

--- example.cs
+++ example.cs
@@ -68,7 +68,7 @@ public class Program
     static byte s_65 = 0;
     static ushort s_66 = 0;
     static uint s_67 = 0;
-    static uint s_68 = 1U;
+    static uint s_68 = 0;
     static sbyte s_69 = 1;
     static ulong s_70 = 9931359137184661811UL;
     static ushort s_71 = 47833;
