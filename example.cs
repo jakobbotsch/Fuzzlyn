3d65990d426afa56e3303d1cefb6ba99bcc02ddc
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:40:24 +0200

    Simplify with RemoveFieldDeclaration

--- example.cs
+++ example.cs
@@ -30,7 +30,6 @@ public class Program
     static long[][, ] s_40 = new long[][, ]{new long[, ]{{0}}};
     static ulong s_41 = 0;
     static uint s_43 = 0;
-    static long s_44 = 0;
     static byte[, ][][][, ] s_45 = new byte[, ][][][, ]{{new byte[][][, ]{new byte[][, ]{new byte[, ]{{0}}}}}};
     static bool s_46 = true;
     static ulong s_47 = 0;
