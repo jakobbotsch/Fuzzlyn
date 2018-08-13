74f908c816db2fbc5b3dcf71a79097771e08f4bc
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:39:25 +0200

    Simplify with SimplifyConstant

--- example.cs
+++ example.cs
@@ -198,7 +198,7 @@ public class Program
     {
         try
         {
-            sbyte[] var2 = new sbyte[]{126};
+            sbyte[] var2 = new sbyte[]{0};
         }
         finally
         {
