579fe5bda6b9e42b535d2d73691d0a5285bc5acd
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:39:45 +0200

    Simplify with SimplifyConstant

--- example.cs
+++ example.cs
@@ -32,7 +32,7 @@ public class Program
     static long s_29 = 0;
     static short[] s_30 = new short[]{0};
     static int s_31 = 0;
-    static int[,, ] s_32 = new int[,, ]{{{1684627006}}};
+    static int[,, ] s_32 = new int[,, ]{{{0}}};
     static uint s_33 = 0U;
     static byte s_34 = 0;
     static long s_35 = 0;
