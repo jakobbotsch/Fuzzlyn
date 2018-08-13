6c6994d3e997bd711968fb439e2cb9866264fd4c
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:40:40 +0200

    Simplify with RemoveMethodArgument

--- example.cs
+++ example.cs
@@ -33,10 +33,10 @@ public class Program
 
     static void M0()
     {
-        byte var0 = (byte)M1(0, ref s_60, s_47, ref s_78, (byte)M81(), s_77);
+        byte var0 = (byte)M1(ref s_60, s_47, ref s_78, (byte)M81(), s_77);
     }
 
-    static uint M1(sbyte arg7, ref byte arg8, ulong arg9, ref sbyte[] arg10, byte arg11, long arg12)
+    static uint M1(ref byte arg8, ulong arg9, ref sbyte[] arg10, byte arg11, long arg12)
     {
         M2(ref s_22);
         return 0;
