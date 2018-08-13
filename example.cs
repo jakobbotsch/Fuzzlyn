8abbb5e46bc2d5c95a276d6b52eb5086b2679a8c
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:40:08 +0200

    Simplify with SimplifyPostfixUnaryExpression

--- example.cs
+++ example.cs
@@ -133,7 +133,7 @@ public class Program
 
     static void M0()
     {
-        byte var0 = (byte)M1(0, 0, (byte)(s_113++), 0, ref s_117, ref s_9[0], ref s_26, 37, ref s_60, s_47, ref s_78, (byte)M81(), s_77++);
+        byte var0 = (byte)M1(0, 0, (byte)(s_113), 0, ref s_117, ref s_9[0], ref s_26, 37, ref s_60, s_47, ref s_78, (byte)M81(), s_77++);
     }
 
     static uint M1(byte arg0, uint arg1, byte arg2, long arg3, ref sbyte[][] arg4, ref sbyte arg5, ref sbyte arg6, sbyte arg7, ref byte arg8, ulong arg9, ref sbyte[] arg10, byte arg11, long arg12)
