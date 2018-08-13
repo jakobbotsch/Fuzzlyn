9bcf2dcb8292c745b5b7c211f90e54a0e261f1ee
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-08-13 21:40:28 +0200

    Simplify with RemoveFieldInitializer

--- example.cs
+++ example.cs
@@ -23,7 +23,7 @@ public class Program
     static long s_29;
     static uint s_33;
     static byte s_34 = 0;
-    static long s_35 = 0;
+    static long s_35;
     static long[][, ] s_40 = new long[][, ]{new long[, ]{{0}}};
     static ulong s_41 = 0;
     static uint s_43 = 0;
