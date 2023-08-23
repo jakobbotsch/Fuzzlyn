ee7072f8d85157bf28ca53e1ebf409045a9a722f
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2023-08-23 12:32:31 +0200

    Add a known error

--- Fuzzlyn/KnownErrors.cs
+++ Fuzzlyn/KnownErrors.cs
@@ -30,5 +30,6 @@ internal class KnownErrors
             "inVarToRegMap[varIndex] == REG_STK",
             "interval->isSpilled",
             "!foundMismatch",
+            "!\"Bad CastToType() in gtFoldExprConst() for a cast from long\"",
         });
 }
